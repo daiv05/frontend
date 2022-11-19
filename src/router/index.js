f7da62ee4280e2b831402b2e44b33d1b3729286d
David Alejandro Deras Cerros <dc19019@ues.edu.sv>
2022-11-19 16:26:54 -0600

    initial project v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DepartamentoView from '../views/DepartamentoView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/departamento',
+    name: 'departamento',
+    component: DepartamentoView
   },
   {
     path: '/about',
