ad32ad1de1b501747b5cc91463af2d14a8b925a1
dennisrmz <orellanadennis12@gmail.com>
2022-11-28 22:45:55 -0600

    prfile

--- src/views/ProfileForm.vue
+++ src/views/ProfileForm.vue
@@ -1,24 +1,10 @@
 <template>
-    <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 
     <body class="antialiased font-sans bg-gray-200 overflow-hidden">
         <div class="" style="">
             <div class="bg-gray-100">
                 <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    <form action="#" method="POST">
+                    <form @submit.prevent="submit">
                         <div class="mt-10 sm:mt-0">
                             <div class="md:grid md:grid-cols-3 md:gap-6">
                                 <div class="md:col-span-1">
@@ -192,10 +178,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                                            <button type="submit"
-                                                class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
-                                        </div>
+
                                     </div>
                                 </div>
                             </div>
@@ -227,7 +210,7 @@
                                                     <label for="telefono"
                                                         class="block text-sm font-medium text-gray-700">Ingrese número
                                                         de telefono</label>
-                                                    <input type="text" name="telefono" id="telefono"
+                                                    <input type="text" v-model="userData.telefono" id="telefono"
                                                         autocomplete="given-name"
                                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                 </div>
@@ -235,7 +218,7 @@
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="whatsapp"
                                                         class="block text-sm font-medium text-gray-700">WhatsApp</label>
-                                                    <input type="text" name="whatsapp" id="whatsapp"
+                                                    <input type="text" v-model="userData.whatsapp" id="whatsapp"
                                                         autocomplete="family-name"
                                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                 </div>
@@ -247,7 +230,7 @@
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="facebook"
                                                         class="block text-sm font-medium text-gray-700">Facebook</label>
-                                                    <input type="text" name="facebook" id="facebook"
+                                                    <input type="text" v-model="userData.facebook" id="facebook"
                                                         autocomplete="family-name"
                                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                 </div>
@@ -255,7 +238,7 @@
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="instagram"
                                                         class="block text-sm font-medium text-gray-700">Instagram</label>
-                                                    <input type="text" name="instagram" id="instagram"
+                                                    <input type="text" v-model="userData.instagram" id="instagram"
                                                         autocomplete="email"
                                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                 </div>
@@ -265,14 +248,17 @@
                                                 <div class="col-span-6">
                                                     <label for="twitter"
                                                         class="block text-sm font-medium text-gray-700">Twitter</label>
-                                                    <input type="text" name="twitter" id="twitter"
+                                                    <input type="text" v-model="userData.twitter" id="twitter"
                                                         autocomplete="twitter"
                                                         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-
+                                    <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                                        <button type="submit"
+                                            class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -284,7 +270,7 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 export default {
     data() {
         return {
@@ -302,7 +288,11 @@ export default {
                 state: '',
                 postalcode: '',
                 about: '',
-
+                telefono: '',
+                whatsapp: '',
+                facebook: '',
+                instagram: '',
+                twitter: '',
             },
             optionsCountry: [
                 { text: 'One', value: 'A' },
@@ -331,6 +321,16 @@ export default {
         },
         removeImage: function (e) {
             this.image = '';
+        },
+        submit() {
+            console.log(this.userData);
+            axios.post('/your-url', { name: this.name })
+                .then(res => {
+                    // do something with res
+                })
+                .catch(err => {
+                    // catch error
+                });
         }
     }
 }
