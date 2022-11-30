c38b8fc3304b792844c0c238d520b2100a35b7a3
juanBM-1122 <100696845+juanBM-1122@users.noreply.github.com>
2022-11-29 21:35:08 -0600

    completo sistema login-logout-register

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ const routes = [
     path : "/login",
     name : "login",
     component : () => import('../views/ViewFormLogin.vue')
+  },
+  {
+    path : "/register",
+    name : "register",
+    component : () => import("../views/RegisterView.vue")
   }
 ]
 
