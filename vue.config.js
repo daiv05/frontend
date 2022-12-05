c518c28fdb383cde01d4cb442c8e37303c1dc03e
David Alejandro Deras Cerros <dc19019@ues.edu.sv>
2022-12-04 23:59:36 -0600

    add static vue

--- vue.config.js
+++ vue.config.js
@@ -30,4 +30,7 @@ module.exports = defineConfig({
       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     }
   },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/public/'
+    : '/'
 })
